0fc9730fd903853e7df1ef83aad2a0fadc6d0313
Michael Johnson <mistermichaellll@gmail.com>
2022-08-03 21:08:58 -0400

    add function to get portfolio value

---
+++ R/portfolio_positions.R
@@ -0,0 +1,40 @@
+#' Get the current positions in your portfolio.
+#'
+#' @details
+#' This function returns top-level information about a user's portfolio, including the market value
+#' and the number of shares they own.
+#'
+get_portfolio_positions <- function(account_number){
+    url <- sprintf(
+        "https://api.apexclearing.com/margin-provider/api/v1/positions/%s",
+        account_number)
+
+    response <-
+        suppressMessages(
+        GET(url,
+            add_headers(
+                Cookie = Sys.getenv("APEX_token"),
+                ContentType = "application/json;charset=UTF-8"
+            )
+        ) |>
+        content(as = "text") |>
+        parse_json()
+        )
+
+    portfolio_positions <-
+        response[[1]][["positions"]] |>
+        map_depth(
+            1, ~tibble(
+                "symbol" = .x[[1]],
+                "name" = .x[[5]],
+                "num_shares" = .x[[8]],
+                "market_price" = .x[[9]],
+                "market_value" = .x[[10]]
+            )
+        ) |>
+        bind_rows() |>
+        filter(market_value > 0)
+
+
+    return(portfolio_positions)
+}
