49591a4dbf9b5ac37a716d1b9648889250b1c139
Michael Johnson <mistermichaellll@gmail.com>
2022-08-06 14:36:14 -0400

    add utility function

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+#' Helper function to clean up code which generates realized gains and losses.
+#'
+#' @param json_response the API response in JSON format.
+#' @param short_or_long one of either "short" or "long" for the term in which gains are evaluated.
+get_gain_loss_by_term <- function(json_response,
+                                  short_or_long = c("short", "long")){
+    short_or_long <- switch(
+        short_or_long,
+        "short" = "stGainLoss",
+        "long" = "ltGainLoss"
+    )
+
+    response_json |>
+        pluck("lot") |>
+        map(
+            ~pluck(.x, short_or_long)
+        ) |>
+        unlist()
+}
