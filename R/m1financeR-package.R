e438bda53e630740b0e4981c404f2b1ecd43567f
Michael Johnson <mistermichaellll@gmail.com>
2022-11-11 17:01:26 -0500

    add imports and exports here

---
+++ R/m1financeR-package.R
@@ -0,0 +1,10 @@
+#' @importFrom httr GET POST content add_headers
+#' @importFrom dplyr bind_rows filter tibble pull mutate case_when relocate
+#' @importFrom purrr map_depth flatten_df flatten_dfc map_df map_dfr map flatten pluck
+#' @importFrom jsonlite parse_json
+#' @export get_portfolio_positions
+#' @export get_open_positions
+#' @export get_realized_gains_losses
+#' @export get_account_activity
+#' @export get_APEX_auth_token
+NULL
