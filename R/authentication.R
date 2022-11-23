dbe858187b98dfda1095232aa7085825ce7faac5
Michael Johnson <mistermichaellll@gmail.com>
2022-11-23 14:18:51 -0500

    update authentication
    Decided this was worth doing in a drive-by

--- R/authentication.R
+++ R/authentication.R
@@ -1,3 +1,32 @@
+#' Get credentials to obtain an APEX auth token.
+#'
+#' @details
+#' This function checks whether a session is interactive. If it is, it prompts the
+#' user to enter their username and password. Otherwise, it checks .Renviron for the
+#' credentials.
+#'
+#' @return
+#' a named list with a user's credentials
+get_credentials <- function() {
+  if (interactive()) {
+    username <- askForSecret("APEX Username")
+    password <- askForSecret("APEX Password")
+  }
+  else {
+    username <- Sys.getenv("APEX_USER")
+    password <- Sys.getenv("APEX_PASS")
+  }
+
+  if (username == "" & password == ""){
+    stop("Could not find username and password. Please set up the following environment variables for non-interactive sessions: APEX_USER, APEX_PASS")
+  }
+
+  list(
+    "username" = username,
+    "password" = password
+  )
+}
+
 #' Get an authorized token to use during your R session.
 #'
 #' A token is required in order to make requests, this token is hidden in the cookies generated when a user logs into their account.
@@ -16,18 +45,15 @@
 #' get_APEX_auth_token() # RStudio prompts the user for their login
 #' }
 get_APEX_auth_token <- function(username, password){
-  if(missing(username) | missing(password)){
-    username <- askForSecret("APEX Username")
-    password <- askForSecret("APEX Password")
-  }
+  credentials <- get_credentials()
 
   url <- "https://api.apexclearing.com/legit/api/v2/session"
   request_body <-
     paste0(
       '{"user":"","password":"',
-      password,
+      credentials$password,
       '","username":"',
-      username,
+      credentials$username,
       '"}'
     )
 
