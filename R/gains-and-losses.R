c7a0d4bdc6cdd659e67d8bb43d6e86bfda9671f6
Michael Johnson <mistermichaellll@gmail.com>
2022-11-23 15:41:11 -0500

    no real need for this to be in utils

--- R/gains-and-losses.R
+++ R/gains-and-losses.R
@@ -1,3 +1,26 @@
+#' Helper function to clean up code which generates realized gains and losses.
+#'
+#' @param json_response the API response in JSON format.
+#' @param short_or_long one of either "short" or "long" for the term in which gains are evaluated.
+#'
+get_gain_loss_by_term <- function(
+    json_response,
+    short_or_long = c("short", "long")
+) {
+  short_or_long <- switch(
+    short_or_long,
+    "short" = "stGainLoss",
+    "long" = "ltGainLoss"
+  )
+
+  json_response |>
+    pluck("lot") |>
+    map(
+      ~pluck(.x, short_or_long)
+    ) |>
+    unlist()
+}
+
 #' Get realized gains and losses for a given portfolio.
 #'
 #' @param account_number a user's M1 Finance account number.
