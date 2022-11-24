72420699ad2a0aea0921a9784af500cd8ebad802
Michael Johnson <mistermichaellll@gmail.com>
2022-11-24 11:57:15 -0500

    Update account-activity.R

--- R/account-activity.R
+++ R/account-activity.R
@@ -5,19 +5,21 @@
 #' @param end_date a string containing the end date for the activity export.
 #'
 get_account_activity <- function(account_number, start_date, end_date) {
-  url <- paste0(
-    "https://api.apexclearing.com/activities-provider/api/v1/activities/",
-    account_number,
-    "?activityType=TRADES&activityType=MONEY_MOVEMENTS&activityType=POSITION_ADJUSTMENTS&",
-    "endDate=", end_date,
-    "&startDate=", start_date
-  )
+  url <- "https://api.apexclearing.com/activities-provider/api/v1/activities/"
 
   response <-
     GET(
       url,
       add_headers(
         Cookie = Sys.getenv("APEX_token")
+      ),
+      query = list(
+        account = account_number,
+        activityType = "TRADES",
+        activityType = "MONEY_MOVEMENTS",
+        activityType = "POSITION_ADJUSTMENTS",
+        endDate = end_date,
+        startDate = start_date
       )
     )
 
